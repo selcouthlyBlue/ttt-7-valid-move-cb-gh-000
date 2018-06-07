195c8f9e9ce14bf0ce8c35c3ec93de9397810e3f
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 05:36:53 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def valid_move?(board, index)
 end
 
 def invalid_position?(board, index)
-  return index > board.length
+  return between(0, board.length - 1)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
